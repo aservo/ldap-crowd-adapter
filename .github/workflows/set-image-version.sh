9319fbde51d1ba1f7bb132475cd254de9d988374
Eric Löffler <eloeffler@aservo.com>
2023-04-12 08:19:46 +0200

    Refactor pipeline and change versioning schema

--- .github/workflows/set-image-version.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-
-VERSION="$(mvn --batch-mode help:evaluate -Dexpression=project.version | grep -v -E '^\[([A-Za-z])+\]')"
-
-echo "IMAGE_VERSION=$VERSION" >> $GITHUB_ENV
-
-exit 0
